5214817bf6ec756e5befd3b5e99cdd1fc55fde93
t-nishimura <hbvcg00@gmail.com>
2022-06-07 16:57:27 +0900

    #15 launchファイルにcontroller系追加

--- launch/wamv_description.launch.py
+++ launch/wamv_description.launch.py
@@ -15,6 +15,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 import launch
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
 import launch_ros.actions
 import xacro
 
@@ -25,16 +28,81 @@ urdf_path = os.path.join(share_dir_path, 'urdf', 'wamv.urdf')
 
 def generate_launch_description():
     enable_dummy = LaunchConfiguration('enable_dummy', default=False)
-    enable_dummy_arg = DecrareLaunchArgment(
+    enable_dummy_arg = DeclareLaunchArgument(
         'enable_dummy', default_value=enable_dummy, description="if true, enable dummy wam-v.")
+    controller_config = os.path.join(get_package_share_directory(
+        'wamv_description'), 'config', 'controllers.yaml')
     doc = xacro.process_file(xacro_path)
     robot_desc = doc.toprettyxml(indent='  ')
     f = open(urdf_path, 'w')
     f.write(robot_desc)
     f.close()
+
     rsp = launch_ros.actions.Node(package='robot_state_publisher',
                                   executable='robot_state_publisher',
                                   output='both',
-                                  arguments=[urdf_path])
+                                  arguments=[urdf_path],
+                                  condition=UnlessCondition(enable_dummy),
+                                  parameters=[{'robot_description': robot_desc}])
 
-    return launch.LaunchDescription([rsp, enable_dummy_arg])
+    control_node = launch_ros.actions.Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_desc}, controller_config],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
+        condition=UnlessCondition(enable_dummy))
+
+    rsp_dummy = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        condition=IfCondition(enable_dummy),
+        arguments=[urdf_path],
+        parameters=[{'robot_description': robot_desc}])
+
+    control_node_dummy = launch_ros.actions.Node(
+        package='controller_manager',
+        executable='ros2_control_node',
+        parameters=[{'robot_description': robot_desc}, controller_config],
+        output={
+            'stdout': 'screen',
+            'stderr': 'screen',
+        },
+        condition=IfCondition(enable_dummy)
+    )
+
+    return launch.LaunchDescription([rsp,
+                                     rsp_dummy,
+                                     enable_dummy_arg,
+                                     control_node,
+                                     control_node_dummy,
+                                     ExecuteProcess(
+                                         cmd=[
+                                             'ros2',
+                                             'control',
+                                             'load_start_controller',
+                                             'joint_state_controller'],
+                                         output='screen',
+                                         shell=True,
+                                     ),
+                                     ExecuteProcess(
+                                         cmd=[
+                                             'ros2',
+                                             'control',
+                                             'load_start_controller',
+                                             'usv_joy_controller'],
+                                         output='screen',
+                                         shell=True,
+                                     ),
+                                     ExecuteProcess(
+                                         cmd=[
+                                             'ros2',
+                                             'control',
+                                             'load_start_controller',
+                                             'dinamixel_diagnostic_controller'],
+                                         output='screen',
+                                         shell=True,
+                                     )])
