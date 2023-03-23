c789c9bba868886681812e5f6d362424c64f0eb5
Masaya Kataoka <ms.kataoka@gmail.com>
2023-03-23 10:57:55 +0900

    fix error
    Signed-off-by: Masaya Kataoka <ms.kataoka@gmail.com>

--- script/tcp_server.py
+++ script/tcp_server.py
@@ -1,7 +1,22 @@
+# Copyright (c) 2020 OUXT Polaris
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0", 12345))  # IPとポート番号を指定します
+s.bind(("0.0.0.0", 12345))  # Specify IP address and port
 s.listen(5)
 
 while True:
